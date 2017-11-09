13f03d84b420193b502c8297b578d0efc27d42a2
kamillo1000 <kamillo1000@gmail.com>
2017-11-10 00:13:06 +0100

    Added User Controler with test rout

---
+++ WebApplication/WebApplication/controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace WebApplication.controllers
+{
+    public class UserController : NancyModule
+    {
+        public UserController()
+        {
+            Get["/user"] = parameters => "Hello user.";
+        }
+    }
+}
