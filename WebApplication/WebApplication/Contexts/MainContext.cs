f599dfe30a8efc67d0b1c255f598c1423ba6bfb4
kamillo1000 <kamillo1000@gmail.com>
2017-11-15 18:36:53 +0100

    Added places to context.

--- WebApplication/WebApplication/Contexts/MainContext.cs
+++ WebApplication/WebApplication/Contexts/MainContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common;
 using WebApplication.Models;
 
 namespace WebApplication.Contexts
@@ -14,5 +15,6 @@ namespace WebApplication.Contexts
         }
 
         public DbSet<ModelUser> Users { get; set; }
+        public DbSet<ModelPlace> Places { get; set; }
     }
 }
